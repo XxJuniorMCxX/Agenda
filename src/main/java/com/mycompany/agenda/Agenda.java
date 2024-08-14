8470627bfbd5709d933761f5f7bd136d66085208
junior_Miguel <juniorcastilloy@gmail.com>
2024-08-13 22:26:14 -0500

    First--App-Agenda

---
+++ src/main/java/com/mycompany/agenda/Agenda.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agenda;
+
+import com.mycompany.agenda.igu.AgendaElectronica;
+
+/**
+ *
+ * @author DREP
+ */
+public class Agenda {
+
+    public static void main(String[] args) {
+        AgendaElectronica agenda_E = new AgendaElectronica();
+        agenda_E.setTitle("contactos Agenda Electronica ");
+        agenda_E.setVisible(true);
+        agenda_E.setLocationRelativeTo(null);
+        
+        //Agregar
+        
+    }
+}
